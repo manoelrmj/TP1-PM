31efb99fe882b6e3c080df75c110002f309c6d81
Manoel <manoelrmiranda@gmail.com>
2017-04-13 20:47:01 -0300

    Leitura de jogadas

--- Main.java
+++ Main.java
@@ -8,20 +8,12 @@ public class Main {
 	private static BufferedReader readArq;
 	private static FileReader arq;
 	
-	public static void main(String[] args) {
-		String path = "testes-alunos/entrada/tabuleiro.txt";
-		String line = "";		
-
-		// Variaveis para criação do tabuleiro
-		Tabuleiro board = new Tabuleiro();
+	public static ArrayList<String> readBoardFile(String boardFilePath){
+		String line = "";
 		ArrayList<String> boardDescription = new ArrayList<String>();
-		Posicao tmpPosition;
-		Imovel tmpProperty = null;
-		int id, position, type, propertyType, propertyValue, propertyTax;
-		
-		// Manipulação de arquivo
+		// Manipulação do arquivo tabuleiro
 		try {
-			arq = new FileReader(path);
+			arq = new FileReader(boardFilePath);
 			readArq = new BufferedReader(arq);
 			while(line != null){
 				line = readArq.readLine();
@@ -36,13 +28,56 @@ public class Main {
 				System.out.println("Erro ao fechar o arquivo tabuleiro.txt");
 			}
 		}
+		return boardDescription;
+	}
+
+	public static ArrayList<String> readMovesFile(String movesFilePath){
+		ArrayList<String> movesDescription = new ArrayList<String>();
+		String line = "";
+		// Manipulação do arquivo jogadas
+		try {
+			arq = new FileReader(movesFilePath);
+			readArq = new BufferedReader(arq);
+			while(line != null){
+				line = readArq.readLine();
+				System.out.println(line);
+				movesDescription.add(line);
+			}
+		} catch (IOException e) {
+			System.out.println("Erro ao ler o arquivo jogadas.txt");
+		} finally { // Garantia de que sempre será feita a tentativa de fechar o arquivo
+			try {
+				arq.close();
+			} catch (IOException e) {
+				System.out.println("Erro ao fechar o arquivo jogadas.txt");
+			}
+		}
+		return movesDescription;
+	}
+	
+	public static void main(String[] args) {
+		// Leitura dos aquivos
+		String boardFilePath = "testes-alunos/entrada/tabuleiro.txt";
+		String movesFilePath = "testes-alunos/entrada/jogadas.txt";
+		ArrayList<String> boardDescription = readBoardFile(boardFilePath);
+		ArrayList<String> movesDescription = readMovesFile(movesFilePath);
 		
+		// Variaveis para criação do tabuleiro e de jogadas		
+		String[] positionInput;
+		Posicao tmpPosition;
+		Imovel tmpProperty = null;
+		int id, position, type, propertyType, propertyValue, propertyTax;
+		String[] movesInfo;
+		int numMoves, numPlayers, initialBalance;
+		
+		Tabuleiro board = new Tabuleiro();
 		board.setNumPositions(Integer.parseInt(boardDescription.get(0)));
 		
+		// Construcao do tabuleiro
 		for (int i=1; i<boardDescription.size(); i++){
 			if(boardDescription.get(i) == null)
 				break;
-			String[] positionInput = boardDescription.get(i).split(";");
+			positionInput = boardDescription.get(i).split(";");
 			id = Integer.parseInt(positionInput[0]);
 			position = Integer.parseInt(positionInput[1]);
 			type = Integer.parseInt(positionInput[2]);
@@ -74,6 +109,22 @@ public class Main {
 					
 				tmpPosition = new Posicao(id, position, type, tmpProperty);
 				board.addPosition(tmpPosition);
+			}
+		}
+		
+		// Leitura das jogadas
+		movesInfo = movesDescription.get(0).split("%"); 
+		numMoves = Integer.parseInt(movesInfo[0]);
+		numPlayers = Integer.parseInt(movesInfo[1]);
+		initialBalance = Integer.parseInt(movesInfo[2]);
+		
+		for (int i=1; i<movesDescription.size(); i++){
+			if(movesDescription.get(i) == null){ 
+				break;
+			}else if(movesDescription.get(i).equals("DUMP")){
+				System.out.println("DUMP"); // DUMP results to file
+			}else{
+				// Executar jogada
 			}
 		}
 		
